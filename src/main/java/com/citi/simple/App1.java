b8d846c0bc4e10389bf5b2bb66d8807b25062e73
Aditi <aditi@practice.com>
2022-05-18 14:07:30 +0530

    Pushing to github

---
+++ src/main/java/com/citi/simple/App1.java
@@ -0,0 +1,14 @@
+package com.citi.simple;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("printing system property");
+		String country=System.getProperty("country");
+		System.out.println("country is "+country);
+
+	
+	}
+
+}
